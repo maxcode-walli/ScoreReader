5e9379abbdc577985efc1454a441b887ef13b649
Alexandra Savescu <alexandrasavescu92@gmail.com>
2023-03-07 15:39:58 +0200

    -MarkTransactionAsAnomaly search for correct object to update in firestore - Update only if anomaly

--- ConsoleApp1/FirestoreService.cs
+++ ConsoleApp1/FirestoreService.cs
@@ -9,18 +9,31 @@ namespace ScoreReader
 {
     public class FirestoreService
     {
-        public async Task MarkTransactionAsAnomalyAsync(TransactionScore transaction)
+        public async Task MarkTransactionAsAnomalyAsync(TransactionScore data)
         {
+            if (data.Label != "High risk") 
+                return;
             try
             {
                 FirestoreDb db = FirestoreDb.Create("impactful-shard-374913");
+                var path = $"users/{data.UserID}/accounts";
 
-                var path = $"users/{transaction.UserID}/accounts/{transaction.ExternalAccountID}/transactions/{transaction.TransactionID}";
-                var doc = await db.Document(path).GetSnapshotAsync();
-                if (doc != null && transaction.Label == "High risk")
+                var accountsQuery = db.Collection(path).WhereEqualTo("externalAccountId", data.ExternalAccountID);
+                var snapshot = await accountsQuery.GetSnapshotAsync();
+                var account = snapshot.Documents.SingleOrDefault();
+
+                if (account != null) 
                 {
-                    var updates = new Dictionary<string, object>() { { "IsAnomaly", true } };
-                    await db.Document(path).UpdateAsync(updates);
+                    var transactionsQuery = db.Collection(path + $"/{account.Id}/transactions")
+                        .WhereEqualTo("Uuid", data.TransactionID);
+                    snapshot = await transactionsQuery.GetSnapshotAsync();
+                    var transaction = snapshot.Documents.SingleOrDefault();
+
+                    if (transaction != null) 
+                    {
+                        var transactionDocument = db.Document(path + $"/{account.Id}/transactions/{transaction.Id}");
+                        await transactionDocument.UpdateAsync("IsAnomaly", true);
+                    }
                 }
             }
             catch (Exception ex)
